e01bd8fa43a10ab880321b3c33fc67ee7aa3076a
TheGabro <gbottani19@gmail.com>
2024-09-30 21:21:35 +0200

    init updated

--- Package/GGH_crypto/__init__.py
+++ Package/GGH_crypto/__init__.py
@@ -1,3 +1,3 @@
-from .GGH.GGH import GGHCryptosystem
-from .GGH_HNF.GGH_HNF import GGHHNFCryptosystem
-from .Utils.Utils import Utils
+from GGH_crypto.GGH.GGH import GGHCryptosystem
+from GGH_crypto.GGH_HNF.GGH_HNF import GGHHNFCryptosystem
+from GGH_crypto.Utils.Utils import Utils
